842ce4c63553792088f59c2fcf4486459bb76e58
VincentSprenger <sprengervincent@gmail.com>
2026-01-22 20:35:24 +0100

    Added the aspect ratio test, to do so put more stuff in the sim class so it can be ran from there also split a run function and animate function to test with.

--- Phase1.py
+++ Phase1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.cm as cm
 import matplotlib.colors as mcolors
+from tqdm import tqdm
 
 # --- CONFIGURATION ---
 room_size=(12.0,12.0)
@@ -21,14 +22,20 @@ B = 0.08
 K = 120000.0          # Body Force (Compression)
 
 class CrowdSimulation:
-    def __init__(self):
+    def __init__(self, room_size=room_size, timestep=0.04, n_individuals=n_individuals):
         # Spawn agents
+        self.room_size = room_size
+        self.dt = timestep
+        self.n_individuals = n_individuals
         self.pos = np.random.rand(n_individuals, 2)
-        self.pos[:, 0] = self.pos[:, 0] * (room_size[0] - 3) + 0.5 
-        self.pos[:, 1] = self.pos[:, 1] * (room_size[1] - 1) + 0.5 
+        self.pos[:, 0] = self.pos[:, 0] * (self.room_size[0] - 3) + 0.5 
+        self.pos[:, 1] = self.pos[:, 1] * (self.room_size[1] - 1) + 0.5 
         self.radius= np.random.normal(loc=radius, scale=0.05, size=n_individuals)
         self.vel = np.zeros((n_individuals, 2))
         self.forces_magnitude = np.zeros(n_individuals)
+        self.exited = 0
+        self.time = 0.0
+        
         # print("Initialized positions:", self.pos)
     
     def get_forces(self):
@@ -36,10 +43,10 @@ class CrowdSimulation:
         pressure_force = np.zeros(n_individuals)
         
         # 1. Goal Direction
-        target = np.array([room_size[0], room_size[1]/2])
+        target = np.array([self.room_size[0], self.room_size[1]/2])
         direction = target - self.pos
-        escaped_mask = self.pos[:, 0] > room_size[0]
-        
+        escaped_mask = self.pos[:, 0] > self.room_size[0]
+
         dist_to_target = np.linalg.norm(direction, axis=1, keepdims=True)
         e_desired = direction / (dist_to_target + 1e-6) # versor
         e_desired[escaped_mask] = np.array([1.0, 0.0]) # Run right if escaped
@@ -94,82 +101,123 @@ class CrowdSimulation:
         new_pos=self.pos+self.vel*dt
        
         #clipping position (stay inside room)
-        new_pos[:, 1] = np.clip(new_pos[:, 1], self.radius, room_size[1] - self.radius)        
+        new_pos[:, 1] = np.clip(new_pos[:, 1], self.radius, self.room_size[1] - self.radius)        
         new_pos[:, 0] = np.maximum(new_pos[:, 0], self.radius)
         
         
         #handle wall collisions (except door)
-        crossing_wall = (new_pos[:, 0] > (room_size[0] - self.radius)) & (self.pos[:, 0] < room_size[0])
-        in_door = np.abs(new_pos[:, 1] - room_size[1]/2) < (exit_width/2 - self.radius/2)
+        crossing_wall = (new_pos[:, 0] > (self.room_size[0] - self.radius)) & (self.pos[:, 0] < self.room_size[0])
+        in_door = np.abs(new_pos[:, 1] - self.room_size[1]/2) < (exit_width/2 - self.radius/2)
         
         blocked_indices = crossing_wall & (~in_door)
         
-        new_pos[blocked_indices, 0] = room_size[0] - self.radius[blocked_indices]
+        new_pos[blocked_indices, 0] = self.room_size[0] - self.radius[blocked_indices]
         self.vel[blocked_indices, 0] = 0
-        self.pos = new_pos    
+        self.pos = new_pos 
 
-sim=CrowdSimulation()
-dt = 0.04
+    def animate(self, histogram=True):
+        self.fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 7))
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 7))
+        # --- PLOT 1: ROOM ---
+        ax1.set_xlim(0, self.room_size[0] + 4)
+        ax1.set_ylim(0, self.room_size[1])
+        ax1.set_aspect('equal')
+        ax1.set_title(f"Crowd Evacuation (N={n_individuals})")
 
-# --- PLOT 1: ROOM ---
-ax1.set_xlim(0, room_size[0] + 4)
-ax1.set_ylim(0, room_size[1])
-ax1.set_aspect('equal')
-ax1.set_title(f"Crowd Evacuation (N={n_individuals})")
-
-# WALLS
-ax1.plot([room_size[0], room_size[0]], 
-         [(room_size[1] - exit_width) / 2, (room_size[1] + exit_width) / 2], 
-         color='green', linewidth=5)
-ax1.plot([0, room_size[0]], [0, 0], color='black')
-ax1.plot([0, room_size[0]], [room_size[1], room_size[1]], color='black')
-ax1.plot([0, 0], [0, room_size[1]], color='black')
-ax1.plot([room_size[0], room_size[0]], [0, (room_size[1] - exit_width) / 2], color='black')
-ax1.plot([room_size[0], room_size[0]], [(room_size[1] + exit_width) / 2, room_size[1]], color='black')  
+        # WALLS
+        ax1.plot([self.room_size[0], self.room_size[0]], 
+                [(self.room_size[1] - exit_width) / 2, (self.room_size[1] + exit_width) / 2], 
+                color='green', linewidth=5)
+        ax1.plot([0, self.room_size[0]], [0, 0], color='black')
+        ax1.plot([0, self.room_size[0]], [self.room_size[1], self.room_size[1]], color='black')
+        ax1.plot([0, 0], [0, self.room_size[1]], color='black')
+        ax1.plot([self.room_size[0], self.room_size[0]], [0, (self.room_size[1] - exit_width) / 2], color='black')
+        ax1.plot([self.room_size[0], self.room_size[0]], [(self.room_size[1] + exit_width) / 2, self.room_size[1]], color='black')  
 
 
-# PEOPLE
-scatter = ax1.scatter(sim.pos[:, 0], sim.pos[:, 1], s=400*sim.radius, c='b', edgecolors='black', linewidth=0.5)
+        # PEOPLE
+        self.scatter = ax1.scatter(self.pos[:, 0], self.pos[:, 1], s=400*self.radius, c='b', edgecolors='black', linewidth=0.5)
 
-# COLORBAR
-norm = mcolors.Normalize(vmin=0, vmax=3000)
-cmap = cm.jet
-sm = cm.ScalarMappable(cmap=cmap, norm=norm)
-sm.set_array([])
-cbar = fig.colorbar(sm, ax=ax1, orientation='horizontal', fraction=0.04, pad=0.1, shrink=0.7)
-cbar.set_label('Pressure Force (Newton)', labelpad=5)
+        # COLORBAR
+        self.norm = mcolors.Normalize(vmin=0, vmax=3000)
+        self.cmap = cm.jet
+        self.sm = cm.ScalarMappable(cmap=self.cmap, norm=self.norm)
+        self.sm.set_array([])
+        self.cbar = self.fig.colorbar(self.sm, ax=ax1, orientation='horizontal', fraction=0.04, pad=0.1, shrink=0.7)
+        self.cbar.set_label('Pressure Force (Newton)', labelpad=5)
 
-# --- PLOT 2: HISTOGRAM (Filtered) ---
-# We start bins at 50 to ignore people with 0 force (Safe)
-# We use fewer bins (15) for wider bars
-ax2.set_xlim(50, 6000)
-ax2.set_ylim(0, 40) # Lower Y limit since we removed the "0-force" majority
-ax2.set_title("Danger Zone: People under Pressure (>50N)")
-ax2.set_xlabel("Crushing Force (N)")
-ax2.set_ylabel("Number of People")
+        # --- PLOT 2: HISTOGRAM (Filtered) ---
+        # We start bins at 50 to ignore people with 0 force (Safe)
+        # We use fewer bins (15) for wider bars
+        ax2.set_xlim(50, 6000)
+        ax2.set_ylim(0, 40) # Lower Y limit since we removed the "0-force" majority
+        ax2.set_title("Danger Zone: People under Pressure (>50N)")
+        ax2.set_xlabel("Crushing Force (N)")
+        ax2.set_ylabel("Number of People")
 
-bins = np.linspace(50, 5000, 8)
-bars = ax2.bar(bins[:-1], np.zeros(len(bins)-1), width=np.diff(bins), align='edge', color='red', alpha=0.7, edgecolor='black')
+        self.bins = np.linspace(50, 5000, 8)
+        self.bars = ax2.bar(self.bins[:-1], np.zeros(len(self.bins)-1), width=np.diff(self.bins), align='edge', color='red', alpha=0.7, edgecolor='black')
 
+    def animate2(self, frame):
+        for _ in range(2): 
+            self.update(self.dt/2)
 
-def animate(frame):
-    for _ in range(2): 
-        sim.update(dt/2)
-    
-    scatter.set_offsets(sim.pos)
-    
-    pressures = sim.forces_magnitude
-    colors = cmap(norm(pressures))
-    scatter.set_color(colors)
-    
-    # Histogram Update
-    counts, _ = np.histogram(sim.forces_magnitude, bins=bins)
-    for bar, count in zip(bars, counts):
-        bar.set_height(count)
+        self.scatter.set_offsets(self.pos)
+
+        pressures = self.forces_magnitude
+        colors = self.cmap(self.norm(pressures))
+        self.scatter.set_color(colors)
         
-    return scatter, list(bars)
+        # Histogram Update
+        counts, _ = np.histogram(self.forces_magnitude, bins=self.bins)
+        for bar, count in zip(self.bars, counts):
+            bar.set_height(count)
 
-anim = FuncAnimation(fig, animate, frames=400, interval=30, blit=False)
-plt.show()
+        return self.scatter, list(self.bars)
+
+    def animate_run(self):
+        anim = FuncAnimation(self.fig, self.animate2, frames=400, interval=30, blit=False)
+        plt.show()
+    
+    def run(self, total_steps=1000):
+        for _ in range(total_steps):
+            self.update(self.dt)
+            self.time += self.dt
+            if min(self.pos[:, 0]) > self.room_size[0]:
+                return self.time
+                #print("All individuals have exited the room.", self.time)
+                #break
+        return np.nan
+
+
+
+
+#sim=CrowdSimulation((48, 3), timestep=0.04, n_individuals=150)
+#sim.animate()
+#sim.animate_run()
+
+#sim.run(total_steps=10000)
+
+
+def AspectRatio(ratio : list[float], runs: int = 20):
+    results = []
+    mean_res = []
+    std_res = []
+    for i in ratio:
+        room_size = (12 * i, 12 / i)
+        runtime = []
+        for _ in tqdm(range(runs)):
+            sim=CrowdSimulation(room_size, timestep=0.04, n_individuals=150)
+            runtime_res = sim.run(total_steps=10000)
+            runtime.append(runtime_res)
+
+        mean_res.append(np.nanmean(runtime))
+        std_res.append(np.nanstd(runtime))
+
+    plt.errorbar(ratio, mean_res, yerr=std_res, marker='o')
+    plt.show()
+
+AspectRatio([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0, 3.0, 4.0])
+
+
+#AspectRatio([1])
